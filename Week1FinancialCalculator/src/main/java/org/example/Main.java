2fa781280f98ea4af56a309685266ca0f0acecbe
codingk5 <Kaylaj790@gmail.com>
2025-04-11 10:59:59 -0400

    Half Way There

--- Week1FinancialCalculator/src/main/java/org/example/Main.java
+++ Week1FinancialCalculator/src/main/java/org/example/Main.java
@@ -4,6 +4,25 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner()
+        Scanner scanner = new Scanner(System.in);
+
+
+
+        System.out.println("What is your current Principal?");
+        int userInput = scanner.nextInt();
+
+        System.out.println("What is your current Intrest Rate?");
+        float userInput1 = scanner.nextFloat();
+
+        System.out.println("What is your current Loan Length");
+        int userInput2 = scanner.nextInt();
+
+
+        //user will be using interger
+
+
+
+
+
     }
 }
