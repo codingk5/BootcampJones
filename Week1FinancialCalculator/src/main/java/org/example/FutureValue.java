124224b0a04e47fe049b0613f16d2c704dfe549c
codingk5 <Kaylaj790@gmail.com>
2025-04-13 00:49:34 -0400

    Cal3

--- Week1FinancialCalculator/src/main/java/org/example/FutureValue.java
+++ Week1FinancialCalculator/src/main/java/org/example/FutureValue.java
@@ -14,22 +14,24 @@ public class FutureValue {
         System.out.println("What is your current interest rate?");
         double r = scanner.nextDouble();
 
+
         System.out.println("What is the number of years?");
         double t = scanner.nextDouble();
 
+      // FV = P( 1 + (r/n)^(n*t)
 
 
-        double n =365;
+        double n = 365;
         double newR = (r / 100) / n;
         double FV = P * Math.pow(1 + newR, n * t);
         double intialPart = P * (1 + newR/n);
         double totalInterest = FV - P;
 
-        System.out.printf("Your current value is %.2f. The interest collected is %f.", totalInterest, FV);
+        System.out.printf("Your current value is %.2f. The interest collected is %.2f.", totalInterest, FV);
 
 
 
-        // FV = P * ( 1 + (r / 365))^(365 * t)
+
 
 
 
---
+++ Week1FinancialCalculator/src/main/java/org/example/ThatPresentValue.java
@@ -0,0 +1,46 @@
+package org.example;
+
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+public class ThatPresentValue {
+    public static void main(String[] args) {
+
+
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What is your monthly payout?");
+        double pmt = scanner.nextDouble();
+
+
+        System.out.println("What is your monthly interest rate?");
+        double r = scanner.nextDouble();
+
+        double newR = (r / 12 / 100);
+
+
+        System.out.println("What is the number of years for payout?");
+        double t = scanner.nextDouble();
+        double n =(t * 12);
+
+        // PV = PMT * [(1 - (1 + r)^(-n)) / r]
+        // double newR = interestRate;
+
+        double w = Math.pow((1 + newR),(-n));
+        double PV = pmt * (1 - w)/newR;
+
+
+        System.out.printf("Your present value is $%.2f", PV);
+
+
+
+        }
+    }
+
+
+
+
+
+
+
