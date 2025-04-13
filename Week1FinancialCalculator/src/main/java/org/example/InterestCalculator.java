246df0cf581205ed1edf48506f4f7785be640d5b
codingk5 <Kaylaj790@gmail.com>
2025-04-13 02:03:33 -0400

    FinalCal1

--- Week1FinancialCalculator/src/main/java/org/example/InterestCalculator.java
+++ Week1FinancialCalculator/src/main/java/org/example/InterestCalculator.java
@@ -7,26 +7,32 @@ public class InterestCalculator {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("What is youe monthly payment?");
+        System.out.println("What is your monthly payment?");
         float monthlyPayment = scanner.nextFloat();
+        double M = monthlyPayment;
 
         System.out.println("What is your principal?");
         float principal = scanner.nextFloat();
+        double P = principal;
 
         System.out.println("What is your annual interest rate?");
         float annualInterestRate = scanner.nextFloat();
+        double r = annualInterestRate;
 
         System.out.println("What is your term length in years?");
         float longTermYears = scanner.nextFloat();
-
-        double M = monthlyPayment;
-        double P = principal;
-        double r = annualInterestRate;
         double y = longTermYears;
-        double n = 12 * y;
-        double i = r / 12;
 
-        double totalInterest = (M * n)-P;
+
+
+
+
+        double n = 12 * y;
+        double totalInterest = r / 12;
+        double k = (M * n) -P;
+
+
+        System.out.printf("You monthly payment is %f. Your total interest is %.2f", k, totalInterest);
 
         }
     }
